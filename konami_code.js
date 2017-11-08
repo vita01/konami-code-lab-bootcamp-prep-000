e7a14f8bce4a325b97d5de8290630165716dbe04
Victoria <vitalina27@gmail.com>
2017-11-08 10:08:08 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,25 +2,19 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let arr=[];
+let  num=0;
   // Write your JavaScript code inside the init() function
 addEventListener("keydown",function(e){
-  arr.push(event.keyCode);
-   if(code.length===arr.length){
-    //alert("Hurray!");
-   //console.log('yes');
-for(var i=0;i<arr.length;i++){
-if(arr[i]==code[i]){
-f*=true;
-}else{f*=0;}
+//  arr.push(event.keyCode);
+  if(arr[num]==code[num]){
+  
+num++;
+}else{num=0;}
 
 
-}
-if(f==true){
-arr.lengh=0;
-alert("Hurray!");
-
-	}else{arr.lengh=0;}
-}
+  if(num==code.length){
+    alert("Hurray!");
+  }
 
 });
 }
