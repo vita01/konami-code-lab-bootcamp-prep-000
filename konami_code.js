0e722e37d2346e0f4249b6afbabf84152f26d9f2
Victoria <vitalina27@gmail.com>
2017-11-08 09:01:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+  let arr=[];
   // Write your JavaScript code inside the init() function
-
+addEventListener("keydown",function(e){
+arr.push(event.keyCode);
+if(code==arr){
+    alert("Hurray!");
 }
+  
+});
+}
+addEventListener("keydown", function(event) {
+   if (event.keyCode == 86)
+     document.body.style.background = "violet";
+ });
+ addEventListener("keyup", function(event) {
+   if (event.keyCode == 86)
+     document.body.style.background = "";
+ });
